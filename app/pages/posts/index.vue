7d61e047cf76937831190455612f89c5a5ad8943
katsukiwataru <a17dc050@dhw.ac.jp>
2019-04-10 21:28:47 +0900

    add index.vue

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container posts-page">
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table
+        :data="showPosts"
+        style="width: 100%"
+        class="table"
+      >
+        <el-table-column
+          prop="title"
+          label="タイトル">
+        </el-table-column>
+        <el-table-column
+          prop="user.id"
+          label="投稿者"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="created_at"
+          label="投稿日時"
+          width="240">
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    showPosts() {
+      return [
+        {
+          id: '001',
+          title: 'How',
+          body: 'Lorem',
+          created_at: '4/9',
+          user: {
+            id: 'potato4d',
+          }
+        },
+        {
+          id: '002',
+          title: 'How',
+          body: 'Lorem',
+          created_at: '4/10',
+          user: {
+            id: 'potato4d',
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+  cursor: pointer;
+}
+</style>
